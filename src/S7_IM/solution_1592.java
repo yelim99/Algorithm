3207f7afd704f9dcac18f4efa7cfd7f16d4b2fee
yelim99 <myl8234@gmail.com>
2024-01-30 16:02:55 +0900

    boj

---
+++ src/S7_IM/solution_1592.java
@@ -0,0 +1,18 @@
+package S7_IM;
+
+import java.util.Scanner;
+
+public class solution_1592 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int l = sc.nextInt();
+		int cnt = 0;
+		
+		
+		
+	}
+}
---
+++ src/Simulation/Solution_1541.java
@@ -0,0 +1,45 @@
+package Simulation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution_1541 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		
+		String[] arr = s.split("-");
+		System.out.println(Arrays.toString(arr));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		String[] arr2 = s.split("[0-9]");
+//		System.out.println(Arrays.toString(arr2));
+		
+//		List<Character> oper = new ArrayList<>();
+//		
+//		
+//		for(int i=0; i<s.length(); i++) {
+//			char c = s.charAt(i);
+//			if (Character.isDigit(c) == false) {
+//				oper.add(c);
+//			}
+//		}
+//		System.out.println(oper);
+		
+		
+	}
+
+}
