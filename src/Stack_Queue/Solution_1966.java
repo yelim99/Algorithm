b268fc9d2cea4c317532ec0e5d34787c7295618f
yelim99 <myl8234@gmail.com>
2024-01-31 17:32:45 +0900

    study_boj

--- src/Stack_Queue/Solution_1966.java
+++ src/Stack_Queue/Solution_1966.java
@@ -1,6 +1,7 @@
 package Stack_Queue;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,36 +15,38 @@ public class Solution_1966 {
 			int n = sc.nextInt();	// 문서의 개수
 			int m = sc.nextInt();	// 인덱스
 			
-			int[] arr = new int[n];	// 중요도
-			int max = 0;
-			int maxIdx = 0;
+			LinkedList<int[]> arr = new LinkedList<>();
+			boolean isMax = true;
+			int count = 0;
 			
-			// 셀렉션 알고리즘
 			for(int j=0; j<n; j++) {
-				arr[i] = sc.nextInt();
-				maxIdx = i;
-				for(int z=j+1; j<n; j++) {
-					if (arr[z] > arr[maxIdx]) {
-						maxIdx = z;
+				arr.add(new int[] {j, sc.nextInt()});
+			}
+			System.out.println(Arrays.toString(arr.get(1)));
+			
+			while (!arr.isEmpty()) {
+				int[] front = arr.poll();
+				
+				
+				System.out.println("front="+Arrays.toString(front));
+				
+				for(int j=0; j<arr.size(); j++) {
+					if (front[1] < arr.get(j)[1]) {
+						arr.offer(front);
+						for(int z=0; z<j; z++) {
+							arr.offer(arr.poll());
+						}
+						isMax = false;
+						break;
 					}
-					int temp = arr[z];
-					arr[z] = arr[maxIdx];
-					arr[maxIdx] = temp;
 				}
+				if (isMax == false) continue;
+				count++;
+				
+				if (front[0] == count) break;
 			}
 			
-			
-			
-			
-			
-//			for(int j=0; j<n-1; j++) {
-//				// 이렇게 하면 처음에만 0번째 검사, 두번째부터는 0-1-.. 검사 안됨
-//				if (impList.get(j) < impList.get(j+1)) {
-//					impList.add(impList.get(j));
-//					impList.remove(j);
-//				}
-//			}
-//			System.out.println(impList);
+			System.out.println(count);
 			
 		}
 		
