c383a74ba54f7bc711e75aed71a8cc1b72d5d245
yelim99 <minyl8234@gmail.com>
2024-03-18 21:35:38 +0900

    update

--- src/W8_BOJ_16234_인구이동/Main.java
+++ src/W8_BOJ_16234_인구이동/Main.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,7 +17,6 @@ public class Main {
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
 	static int sum;
-	static List<Integer> list;
 //	static int days;
 
 	public static void main(String[] args) throws IOException {
@@ -39,7 +40,6 @@ public class Main {
 		population = new int[n][n];
 		visited = new boolean[n][n];
 		
-		list = new ArrayList<>();
 		
 		// 인구 수 입력받아서 저장
 		for(int i=0; i<n; i++) {
@@ -49,68 +49,110 @@ public class Main {
 			}
 		}
 		
+		int result = 0;	// 인구 이동 횟수
 		
+		// 인구이동이 더이상 일어나지 않을 때 반복문을 벗어나기 위한 check
+		boolean check = true;	
 		
-		System.out.println(move());
-		System.out.println(list);
-		
-	}
-
-	// dfs 탐색하면서 국경선 열 수 있는지 확인
-	static void dfs(int r, int c) {
-		if (r<0 || r>=n || c<0 || c>=n) return;
-		
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			
-			// 범위 안에 있고, 차가 조건에 부합하다면 방문 표시
-			if (nr>=0 && nr<n && nc>=0 && nc<n) {
-				int diff = Math.abs(population[r][c]-population[nr][nc]);
-				if (L <= diff && R >= diff) {
-					visited[r][c] = true;
-					list.add(population[r][c]);
-					
-					// 이미 방문한 곳이면 넘어가기
-					if (!visited[nr][nc]) {
-						dfs(nr, nc);
-					}
-				}
-			}
+		while(check) {
+			if (move()==0) check = false;
+			else result++;
 		}
+		
+		System.out.println(result);
+		
 	}
 	
+
+	// bfs 탐색
 	static int move() {
-		int days = 0;
-		while(true) {
-			boolean check = false;
-			visited = new boolean[n][n];
-			
-			for(int i=0; i<n; i++) {
-				for(int j=0; j<n; j++) {
-					if(!visited[i][j]) {
-						dfs(i, j);
-						if (list.size()>1) {
-							for(int x : list) {
-								sum += x;
+		int cnt = 0;	// 연합 형성되면 증가시킬거임.
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				// 방문한 적이 없다면 bfs 탐색
+				if (!visited[i][j]) {
+					Queue<Location> queue = new LinkedList<>();
+					Location location = new Location(i, j);
+					// 큐에 현재 나라의 위치 저장
+					queue.add(location);
+					
+					// 연합이 형성된 나라 위치 저장해둘 리스트
+					// 리스트에도 현재 나라의 위치 저장
+					List<Location> list = new ArrayList<>();
+					list.add(location);
+					
+					// 방문 표시
+					visited[i][j] = true;
+					
+					// 현재 나라 인구 저장
+					sum = population[location.r][location.c];
+					
+					while(!queue.isEmpty()) {
+						// 현재 위치
+						Location cur = queue.poll();
+						
+						for(int d=0; d<4; d++) {
+							int nr = cur.r+dr[d];
+							int nc = cur.c+dc[d];
+							
+							// 범위 안에 있을 때
+							if (nr>=0 && nr<n && nc>=0 && nc<n) {
+								// 인구 차
+								int diff = Math.abs(population[cur.r][cur.c]-population[nr][nc]);
+								
+								// 방문한 적이 없고, 인구 차가 L이상 R이하일 때
+								if (!visited[nr][nc] && L <= diff && R >= diff) {
+									// 큐와 리스트에 각각 현재 위치 담아주기
+									queue.add(new Location(nr, nc));
+									list.add(new Location(nr, nc));
+									// 방문 표시
+									visited[nr][nc] = true;
+									// 연합 형성되었으니 1 증가
+									cnt++;
+									// 해당 나라 인구 더해주기
+									sum += population[nr][nc];
+								}
 							}
-							int avg = sum/list.size();
-							population[i][j] = avg;
-							check = true;
 						}
 					}
+					
+					// 연합이 형성되었으면 인구 분배
+					if (cnt > 0) {
+						// 평균 구해서
+						int avg = sum/list.size();
+						
+						// 리스트 돌면서 인구 분배해주기
+						for(int x=0; x<list.size(); x++) {
+							Location cur = list.get(x);
+							population[cur.r][cur.c] = avg;
+						}
+					}
+					
 				}
 			}
-			if (!check) {
-				return days;
-			}
-			days++;
-			
-			
-			
 		}
 		
+		// visted 배열 초기화
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				visited[i][j] = false;
+			}
+		}
 		
+		return cnt;
+		
+	}
+	
+	// 나라의 위치 저장할 클래스
+	static class Location {
+		int r;
+		int c;
+		
+		public Location(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
 	}
 
 }
---
+++ src/W8_BOJ_4179_불/Main.java
@@ -0,0 +1,10 @@
+package W8_BOJ_4179_불;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
